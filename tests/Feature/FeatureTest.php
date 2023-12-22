7c5180bdd3f49dc39736cdd4812b55c8b22cdd06
tusharkhan <tushar.khan0122@gmail.com>
2023-12-23 04:23:37 +0600

    adding test

---
+++ tests/Feature/FeatureTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * created by: tushar Khan
+ * email : tushar.khan0122@gmail.com
+ * date : 12/23/2023
+ */
+
+
+namespace TusharKhan\FileDatabase\Tests\Feature;
+
+use TusharKhan\FileDatabase\Tests\TestCase;
+
+class FeatureTest extends TestCase
+{
+
+}
