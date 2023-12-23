868389d95544739842e1466a042c516cfca9b451
tusharkhan <tushar.khan0122@gmail.com>
2023-12-23 11:44:54 +0600

    adding some core classes

---
+++ src/Core/MainModel.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * created by: tushar Khan
+ * email : tushar.khan0122@gmail.com
+ * date : 12/23/2023
+ */
+
+
+namespace Tusharkhan\FileDatabase\Core;
+
+use Traversable;
+
+class MainModel implements \IteratorAggregate
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getIterator()
+    {
+        // TODO: Implement getIterator() method.
+    }
+}
