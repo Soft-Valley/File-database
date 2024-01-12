3380da8e0064185819938335e4bc81c23ecc1fc4
tusharkhan <tushar.khan0122@gmail.com>
2024-01-13 01:55:26 +0600

    belongsTo() and belongsToMany() method done

---
+++ tests/Feature/EloquentTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Tusharkhan\FileDatabase\Models\TestModel;
+
+/**
+ * created by: tushar Khan
+ * email : tushar.khan0122@gmail.com
+ * date : 1/13/2024
+ */
+
+
+class EloquentTest extends \TusharKhan\FileDatabase\Tests\TestCase
+{
+    public function test_eloquent_method()
+    {
+        $testModel = new TestModel();
+        $testModel->name = 'tushar';
+        $testModel->char = 't';
+        $testModel->varchar = 'tushar';
+        $testModel->text = 'tushar';
+        $testModel->tinyInt = 11;
+
+
+        $this->assertIsArray($testModel->save());
+    }
+
+    public function test_delete_record()
+    {
+        $testModel = new TestModel();
+
+        $this->assertTrue($testModel->delete(1));
+    }
+
+    public function test_update_record()
+    {
+        $testModel = new TestModel();
+
+        $this->assertTrue($testModel->update(3, [
+            'name' => 'tushar 2',
+            'char' => 'f',
+            'varchar' => 'tushar',
+            'text' => 'tushar',
+            'tinyInt' => 11
+        ]));
+    }
+}
---
+++ tests/Feature/SchemaTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Tusharkhan\FileDatabase\Core\MainClasses\Builder;
+use Tusharkhan\FileDatabase\Core\MainClasses\Schema;
+
+/**
+ * created by: tushar Khan
+ * email : tushar.khan0122@gmail.com
+ * date : 1/13/2024
+ */
+
+
+class SchemaTest extends \TusharKhan\FileDatabase\Tests\TestCase
+{
+    public function test_schema_create()
+    {
+        $dd = Schema::process('test_models', function (Builder $table) {
+            $table->string('name');
+            $table->char('char');
+            $table->varchar('varchar');
+            $table->text('text');
+            $table->tinyInt('tinyInt');
+            $table->primary('test_models_id');
+        })->run();
+
+        $this->assertTrue($dd);
+    }
+
+    public function test_update_table()
+    {
+        $dd = Schema::process('test_table', function (Builder $table) {
+            $table->date('date_new');
+            $table->dateTime('date_time_new');
+            $table->string('name');
+            $table->remove('description');
+        })->update();
+
+        $this->assertTrue($dd);
+    }
+
+    public function test_drop_table()
+    {
+        $dd = Schema::drop('test_models');
+
+        $this->assertTrue($dd);
+    }
+}
